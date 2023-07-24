b16987a2e7d1d01cbca847aaa77fd32769dafeac
Daniel <calderondanieldavid@gmail.com>
2023-07-24 10:00:03 -0500

    Primer commit - Se agrega proyecto PokeApi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,19 +8,11 @@ import { Meta } from '@angular/platform-browser';
 })
 export class HomeComponent {
   constructor(private meta:Meta) {
-    meta.updateTag({property: 'og:image', content: 'https://portafoliodanieldct.netlify.app/assets/logo-portafolio.png'});
-    meta.updateTag({property: 'og:title', content: 'Desarrollador Web | Portafolio | Daniel David Calderón Tinjaca'});
-    meta.updateTag({name:'description', content:'¡Bienvenidos a mi portafolio personal! Explora una colección de mis proyectos personales y habilidades en diversos lenguajes de programación. ¡Espero que disfrutes descubriendo mi trabajo y trayectoria!'});
-    meta.updateTag({name:'copyright', content:'Copyright - Daniel David Calderón Tinjaca'});
+    meta.updateTag({property: 'og:title', content: 'PokeApi | Daniel David Calderón Tinjaca'});
+    meta.updateTag({name: 'description', content:'¡Descubre el emocionante mundo de Pokémon! Información detallada sobre variedad de Pokémon, habilidades, movimientos y evoluciones en esta web con API Pokémon. ¡Conviértete en un Maestro Pokémon!'});
+    meta.updateTag({name: 'copyright', content:'Copyright - Daniel David Calderón Tinjaca'});
     meta.updateTag({name: 'author', content: 'Daniel David Calderón Tinjaca'});
-    meta.updateTag({name:'robots', content:'index, follow'});
+    meta.updateTag({name: 'robots', content:'index, follow'});
     meta.updateTag({name: 'language', content: 'es'});
-  }
-  // Se agrega el siguiente código para realizar el desplazamiento suave al elemento (SECCION) correspondiente
-  scrollToComponent(elementId: string): void {
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
   }
 }
